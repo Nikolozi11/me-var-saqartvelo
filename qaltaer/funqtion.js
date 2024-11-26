663ac28490efd0803450bb69f81a8e9e9e32fa2a
Nikolozi11 <nikolkzgazviani2011@gmail.com>
2024-11-26 22:02:03 +0400

    second commit

--- qaltaer/funqtion.js
+++ qaltaer/funqtion.js
@@ -1,5 +1,5 @@
 
-const matchStartTime = new Date("2024-10-29T21:00:00+04:00").getTime(); // GMT+4
+const matchStartTime = new Date("2025-02-21T00:00:00+04:00").getTime(); // GMT+4
 
 // დანარჩენი კოდი იგივე რჩება
 const countdown = setInterval(function() {

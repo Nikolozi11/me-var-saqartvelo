a2a86c4dc7a045b18a6efcb03cd3b8d1b2cec36c
Nikolozi11 <nikolkzgazviani2011@gmail.com>
2025-06-05 18:03:38 +0400

    second commit

--- qaltaer/funqtion.js
+++ qaltaer/funqtion.js
@@ -31,3 +31,25 @@ const navLinks = document.getElementById('navLinks');
 burger.addEventListener('click', () => {
     navLinks.classList.toggle('active'); // ღილაკზე დაჭერისას მენიუ გამოჩნდება ან დაიმალება
 });
+
+
+
+//navჩამოშლა
+
+document.querySelectorAll('.submenu-parent').forEach((item) => {
+    item.addEventListener('mouseenter', () => {
+        // ჯერ ყველა submenu დავმალოთ
+        document.querySelectorAll('.submenu').forEach((sub) => {
+            sub.style.display = 'none';
+        });
+
+        // მხოლოდ ამ hovered item-ის submenu ვაჩვენოთ
+        const submenu = item.querySelector('.submenu');
+        if (submenu) submenu.style.display = 'block';
+    });
+
+    item.addEventListener('mouseleave', () => {
+        const submenu = item.querySelector('.submenu');
+        if (submenu) submenu.style.display = 'none';
+    });
+});

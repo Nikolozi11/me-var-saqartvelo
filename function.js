66499cbe12a9946a6ae7bb726fbcac54a509ae94
Nikolozi11 <nikolkzgazviani2011@gmail.com>
2025-06-05 17:56:14 +0400

    second commit

--- function.js
+++ function.js
@@ -5,5 +5,27 @@ burger.addEventListener('click', () => {
     navLinks.classList.toggle('active'); // ღილაკზე დაჭერისას მენიუ გამოჩნდება ან დაიმალება
 });
 
+//ჩამოშლა
+
+document.querySelectorAll('.submenu-parent').forEach((item) => {
+    item.addEventListener('mouseenter', () => {
+        // ჯერ ყველა submenu დავმალოთ
+        document.querySelectorAll('.submenu').forEach((sub) => {
+            sub.style.display = 'none';
+        });
+
+        // მხოლოდ ამ hovered item-ის submenu ვაჩვენოთ
+        const submenu = item.querySelector('.submenu');
+        if (submenu) submenu.style.display = 'block';
+    });
+
+    item.addEventListener('mouseleave', () => {
+        const submenu = item.querySelector('.submenu');
+        if (submenu) submenu.style.display = 'none';
+    });
+});
+
+
+
 
 
